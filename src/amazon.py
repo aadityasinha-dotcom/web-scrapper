cb952f5e72a1d6158cce9c239c100dc3800d4589
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-05-24 10:49:17 +0530

    added new files

---
+++ src/amazon.py
@@ -0,0 +1,35 @@
+import csv
+from selectorlib import Extractor
+import requests 
+import json 
+from time import sleep
+
+
+# Create an Extractor by reading from the YAML file
+e = Extractor.from_yaml_file('selectors.yml')
+
+def scrape(url):  
+
+    headers = ({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'})
+
+    # Download the page using requests
+    print("Downloading %s"%url)
+    r = requests.get(url, headers=headers)
+    # Simple check to check if page was blocked (Usually 503)
+    if r.status_code > 500:
+        if "To discuss automated access to Amazon data please contact" in r.text:
+            print("Page %s was blocked by Amazon. Please try using better proxies\n"%url)
+        else:
+            print("Page %s must have been blocked by Amazon as the status code was %d"%(url,r.status_code))
+        return None
+    # Pass the HTML of the page and create 
+    return e.extract(r.text)
+
+# product_data = []
+def scrap(url):
+    with open('results.csv', 'a', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        data = scrape(url) 
+        writer.writerow(['Short Description' ,'Product Description'])
+        if data:
+            writer.writerow([data['short_description'], data['product_description']])
---
+++ src/main.py
@@ -0,0 +1,42 @@
+import csv
+from matplotlib.pyplot import title
+from selectorlib import Extractor
+import requests 
+
+
+# Create an Extractor by reading from the YAML file
+e = Extractor.from_yaml_file('search_results.yml')
+
+def scrape(url):  
+
+    headers = ({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'})
+
+    # Download the page using requests
+    print("Downloading %s"%url)
+    r = requests.get(url, headers=headers)
+    # Simple check to check if page was blocked (Usually 503)
+    if r.status_code > 500:
+        if "To discuss automated access to Amazon data please contact" in r.text:
+            print("Page %s was blocked by Amazon. Please try using better proxies\n"%url)
+        else:
+            print("Page %s must have been blocked by Amazon as the status code was %d"%(url,r.status_code))
+        return None
+    # Pass the HTML of the page and create 
+    return e.extract(r.text)
+
+with open('results.csv', 'w', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(['URL' ,'Title', 'Price', 'Rating', 'ReviewCount'])
+        for page in range(1, 21):
+            url = 'https://www.amazon.in/s?k=bags&crid=2M096C61O4MLT&qid=1653308124&sprefix=ba%2Caps%2C283&ref=sr_pg_{}'.format(page)
+            data = scrape(url)
+            if data:
+                for product in data['products']:
+                    title1 = product['title']
+                    url1 = product['url']
+                    price = product['price']
+                    rating = product['rating']
+                    reviews = product['reviews']
+                    writer.writerow([url1, title1, price, rating, reviews])
+
+                
